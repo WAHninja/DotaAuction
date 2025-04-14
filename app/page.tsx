b9b615fcb14b94e2902795f3bad82c57d85b9093
WAHninja <hodge_w@hotmail.com>
2025-04-14 15:09:32 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Dota Auctions</h1>
+      <p>This is the homepage of your new Next.js + Render app.</p>
+    </main>
+  );
+}
