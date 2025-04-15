ef0356422a34b29022cc9f3d0ff20121515b7522
WAHninja <hodge_w@hotmail.com>
2025-04-15 09:43:13 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-const path = require('path');
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@'] = require('path').resolve(__dirname);
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
