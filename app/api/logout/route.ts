e41927aab44948c702b7306318c792f65257dd89
WAHninja <hodge_w@hotmail.com>
2025-04-15 22:30:00 +0100

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,27 +1,6 @@
 // app/logout/route.ts
-import { NextResponse } from 'next/server';
-import { SESSION_COOKIE_NAME } from '../../../lib/session';
-import { cookies } from 'next/headers';
-import db from '../../../lib/db';
+import { destroySession } from '../../../lib/session';
 
 export async function POST() {
-  const cookieStore = await cookies();
-  const sessionId = cookieStore.get(SESSION_COOKIE_NAME)?.value;
-
-  if (sessionId) {
-    await db.session.deleteMany({
-      where: { id: sessionId },
-    });
-  }
-
-  const res = NextResponse.redirect('/');
-  res.cookies.set(SESSION_COOKIE_NAME, '', {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
-    path: '/',
-    maxAge: 0,
-  });
-
-  return res;
+  return await destroySession();
 }
