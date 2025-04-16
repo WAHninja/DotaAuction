6c6c8675a4a79a38b9d206e52aa00a0ad1f48c38
WAHninja <hodge_w@hotmail.com>
2025-04-16 16:48:57 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,20 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cinzel: ['var(--font-cinzel)'],
+      },
+      colors: {
+        // optional custom theme colors
+        team1: '#1E3A8A', // example deep blue
+        teamA: '#B91C1C', // example crimson red
+        gold: '#FFD700',
+      },
+    },
   },
   plugins: [],
-}
+};
