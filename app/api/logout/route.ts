582cfed7c7e9317880d8474526f5d4d807ba3637
WAHninja <hodge_w@hotmail.com>
2025-04-17 12:24:40 +0100

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { destroySession } from '../../../lib/session';
 
 export async function POST(req: NextRequest) {
-  const response = NextResponse.redirect(new URL('/', req.url));
+  const response = NextResponse.redirect('/');
   await destroySession(response);
   return response;
 }
