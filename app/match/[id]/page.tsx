fc57d4c7132175a65fb362cf104759a38fa9e0bf
WAHninja <hodge_w@hotmail.com>
2025-04-22 13:01:07 +0100

    Update page.tsx

--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-import SelectWinnerForm from '../../components/SelectWinnerForm';
+import SelectGameWinnerForm from '../../components/SelectGameWinnerForm';
 
 export default function MatchPage() {
   const { id } = useParams();
