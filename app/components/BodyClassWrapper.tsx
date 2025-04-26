08162bb66df2962a69f9bff85cac53e236909ab8
WAHninja <hodge_w@hotmail.com>
2025-04-26 21:17:20 +0100

    Update BodyClassWrapper.tsx

--- app/components/BodyClassWrapper.tsx
+++ app/components/BodyClassWrapper.tsx
@@ -8,12 +8,15 @@ export default function BodyClassWrapper() {
 
   useEffect(() => {
     const isAuthPage = pathname === '/register' || pathname === '/login';
+    const ismatchPage = pathname === '/match';
 
     const body = document.body;
     if (isAuthPage) {
       body.classList.add('bg-[url("/bg-smoke.jpg")]', 'bg-cover', 'bg-center', 'bg-no-repeat');
-    } else {
+    if (ismatchPage) {
       body.classList.add('bg-[url("/LastSupper.jpg")]', 'bg-cover', 'bg-center', 'bg-no-repeat');
+    } else {
+      body.classList.add('bg-[url("/header_bg.webm")]', 'bg-cover', 'bg-center', 'bg-no-repeat');
     }
   }, [pathname]);
 
