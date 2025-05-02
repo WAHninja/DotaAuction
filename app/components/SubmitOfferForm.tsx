c99484c0f8fd3e8f6a414e629fe547d2d661b334
WAHninja <hodge_w@hotmail.com>
2025-05-02 16:18:34 +0100

    Update SubmitOfferForm.tsx

--- app/components/SubmitOfferForm.tsx
+++ app/components/SubmitOfferForm.tsx
@@ -64,7 +64,7 @@ export default function SubmitOfferForm({
     }
   };
 
-      const data = await response.json();
+      const data = await Response.json();
       if (res.ok) {
         setMessage('Offer submitted!');
       } else {
