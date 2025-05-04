e5ac0354ee1ab8d09a2cd3ec03fbb7cd230f1845
WAHninja <hodge_w@hotmail.com>
2025-05-04 02:05:40 +0100

    Update ablyClient.ts

--- lib/ablyClient.ts
+++ lib/ablyClient.ts
@@ -1,7 +1,8 @@
 import * as Ably from 'ably/promises';
 
+// Use the secret key for server-side interactions
 const ably = new Ably.Realtime.Promise({
-  key: process.env.NEXT_PUBLIC_ABLY_API_KEY!,
+  key: process.env.ABLY_API_KEY!,  // Secret key for server-side
 });
 
 export default ably;
