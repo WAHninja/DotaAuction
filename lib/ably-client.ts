24bfbbc468ede73f39cbcdcb22395b7125079d89
WAHninja <hodge_w@hotmail.com>
2025-05-04 20:30:35 +0100

    Update ably-client.ts

--- lib/ably-client.ts
+++ lib/ably-client.ts
@@ -2,10 +2,8 @@ import Ably from 'ably';
 
 const getAuthUrl = () => {
   if (typeof window === 'undefined') {
-    // On the server (e.g. SSR or API routes), must use absolute URL
     return process.env.NEXT_PUBLIC_SITE_URL + '/api/ably/token';
   }
-  // In the browser, relative path is okay
   return '/api/ably/token';
 };
 
