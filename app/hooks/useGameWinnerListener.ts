aceaef22bb6ec9d54d61251d5b938ac34a758196
WAHninja <hodge_w@hotmail.com>
2025-05-04 20:04:00 +0100

    Update useGameWinnerListener.ts

--- app/hooks/useGameWinnerListener.ts
+++ app/hooks/useGameWinnerListener.ts
@@ -3,18 +3,29 @@ import ablyClient from '@/lib/ably-client';
 
 export function useGameWinnerListener(matchId: string, onUpdate: () => void) {
   useEffect(() => {
-    const channel = ablyClient.channels.get('match-' + matchId);
+    if (!matchId) return;
 
-    const handler = (msg: any) => {
-      if (msg.name === 'game-winner-selected') {
-        onUpdate(); // Trigger refetch or state update
-      }
+    let channel: any;
+
+    const subscribe = async () => {
+      const client = await ablyClient; // Await the promise
+      channel = client.channels.get('match-' + matchId);
+
+      const handler = (msg: any) => {
+        if (msg.name === 'game-winner-selected') {
+          onUpdate();
+        }
+      };
+
+      channel.subscribe('game-winner-selected', handler);
     };
 
-    channel.subscribe('game-winner-selected', handler);
+    subscribe();
 
     return () => {
-      channel.unsubscribe('game-winner-selected', handler);
+      if (channel) {
+        channel.unsubscribe('game-winner-selected');
+      }
     };
   }, [matchId, onUpdate]);
 }
