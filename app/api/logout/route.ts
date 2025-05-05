73888b11cee9051f1bfca5720ae56380663c18c7
WAHninja <hodge_w@hotmail.com>
2025-05-05 14:26:55 +0100

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { destroySession } from '@/app/session';
 
 export async function POST(req: NextRequest) {
-  const response = NextResponse.redirect('https://dotaauction.onrender.com');
+  const response = NextResponse.redirect(`${req.nextUrl.origin}/login`);
   await destroySession(response);
   return response;
 }
