782a6d0778bae64780e11bf97e4baeafbb5578b4
WAHninja <hodge_w@hotmail.com>
2025-05-09 14:19:43 +0100

    Update TeamCard.tsx

--- app/components/TeamCard.tsx
+++ app/components/TeamCard.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 
-export default function TeamCard({ name, logo, players, teamId }) {
+export default function TeamCard({ name, logo, players, teamId, color }) {
+  const bgClass = color || 'from-green-700 via-green-600 to-green-600';
+
   return (
-    <div className="bg-gradient-to-b from-[rgba(54,83,20,0.6)] via-[rgba(63,98,18,0.6)] to-[rgba(63,98,18,0.6)] p-6 rounded-2xl shadow-lg backdrop-opacity-10">
+    <div className={`bg-gradient-to-b ${bgClass} p-6 rounded-2xl shadow-lg backdrop-opacity-10`}>
       <div className="flex flex-row justify-center mb-4">
         <Image src={logo} alt={`${name} Logo`} width={64} height={64} />
         <h2 className="text-2xl font-semibold ml-4 mt-5">{name}</h2>
