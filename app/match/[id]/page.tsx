92d3b99d83c862aa30aec262a1439879e9d4b438
WAHninja <hodge_w@hotmail.com>
2025-05-09 15:33:42 +0100

    Update page.tsx

--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -242,7 +242,7 @@ export default function MatchPage() {
 <div className="flex flex-col md:flex-row gap-6 items-start">
 
   {/* Shopkeeper Image */}
-  <div className="hidden md:block absolute top-[-60px] left-[-80px] z-10 w-fit">
+  <div className="hidden md:block relative -ml-20 z-10 overflow-visible w-fit">
     <Image
       src="/Shopkeeper.png"
       alt="Shopkeeper"
