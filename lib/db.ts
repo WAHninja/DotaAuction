477cd9a96543593b9b5770efc08e0393da4f9522
WAHninja <hodge_w@hotmail.com>
2025-05-12 16:12:57 +0100

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
