064dd7e287c6061fcea7fe6b46cf6d58f09909dd
WAHninja <hodge_w@hotmail.com>
2025-05-14 13:36:42 +0100

    Update TeamCard.tsx

--- app/components/TeamCard.tsx
+++ app/components/TeamCard.tsx
@@ -7,7 +7,7 @@ export default function TeamCard({ name, logo, players, teamId, color }) {
     <div className={`bg-gradient-to-b ${gradient} p-6 rounded-2xl shadow-lg backdrop-opacity-10`}>
       <div className="flex flex-row justify-center mb-4">
         <Image src={logo} alt={`${name} Logo`} width={100} height={100} />
-        <h2 className="text-2xl font-semibold ml-4 mt-5">{name}</h2>
+        <h2 className="text-2xl font-semibold ml-4 mt-10">{name}</h2>
       </div>
       <ul className="space-y-2">
         {players.map((p) => (
