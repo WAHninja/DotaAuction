67961f3ac51b25843a4fbe637ee63ce58c2e149a
WAHninja <hodge_w@hotmail.com>
2025-05-20 11:13:31 +0100

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -3,10 +3,10 @@ import { destroySession } from '@/app/session';
 
 export async function POST(req: NextRequest) {
   const base = process.env.NODE_ENV === 'production'
-    ? process.env.RENDER_EXTERNAL_URL    // e.g. https://dotaauction.onrender.com
-    : req.nextUrl.origin;
+    ? process.env.RENDER_EXTERNAL_URL
+    : `${req.nextUrl.protocol}//${req.nextUrl.host}`;
 
   const response = NextResponse.redirect(`${base}/login`);
-  await destroySession(response); // This must run BEFORE return
+  await destroySession(response);
   return response;
 }
