c6fd104c71699e350f71cec9b9150ca7a657c695
WAHninja <hodge_w@hotmail.com>
2025-05-20 10:48:11 +0100

    Update LogoutButton.tsx

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -7,7 +7,7 @@ export default function LogoutButton() {
   const router = useRouter();
 
   const handleLogout = async () => {
-    await fetch('/api/logout', { method: 'DELETE' });
+    await fetch('/api/logout', { method: 'POST' });
     router.push('https://dotaauction.onrender.com/login');
   };
 
