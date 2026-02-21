41a36e0a3ed9c1d901f9f767050ee56142985f50
WAHninja <hodge_w@hotmail.com>
2026-02-21 21:49:36 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     config.resolve.alias['@'] = require('path').resolve(__dirname);
 
-    // Ably uses dynamic requires internally which triggers a webpack warning.
-    // Marking it as an external for the server bundle silences this cleanly.
-    if (isServer) {
-      config.externals = [...(config.externals || []), 'ably'];
-    }
-
     return config;
   },
 };
