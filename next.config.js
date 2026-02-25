7cd09fea39431d3d5b5e34b3644138c272542145
WAHninja <hodge_w@hotmail.com>
2026-02-25 16:59:54 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '*.steamstatic.com' },
+      { protocol: 'https', hostname: '*.akamaihd.net' },
+    ],
+  },
   webpack: (config, { isServer }) => {
     config.resolve.alias['@'] = require('path').resolve(__dirname);
-
     return config;
   },
 };
