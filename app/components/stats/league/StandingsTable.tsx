f5f94938da55ef8327a3931c8ba6295f0df508ea
WAHninja <hodge_w@hotmail.com>
2026-09-10 23:57:26 +0100

    Fix formatting and add missing newline at end of file

--- app/components/stats/league/StandingsTable.tsx
+++ app/components/stats/league/StandingsTable.tsx
@@ -162,6 +162,7 @@ export default function StandingsTable({ players, dotaStats, highlightUsername }
                     >
                       <PlayerAvatar
                         username={row.username}
+                        steamAvatar={row.steamAvatar}
                         size={24}
                         className={isYou ? 'ring-2 ring-dota-gold' : ''}
                       />
