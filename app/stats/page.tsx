0cf549d0bbae1953b289028c4aa9b2bfb8e58dd0
WAHninja <hodge_w@hotmail.com>
2026-09-11 10:36:11 +0100

    Update page.tsx

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -46,7 +46,7 @@ export default function StatsPage() {
 
       <YouCard />
 
-      <LeagueVitals payload={payload} />
+      <LeagueVitals totals={payload.leagueTotals} />
 
       <StandingsTable
         players={payload.players}
